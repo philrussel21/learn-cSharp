55ba127897ba8b438677d29f22f027efb4616874
philrussel21 <phil.antiporda21@gmail.com>
2021-03-09 22:27:54 +1000

    Inits another class called Book. adds a method inside the Book. instantiate Book class inside Program.cs file

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Program
+  class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello {args[0]}!");
+            var book = new Book();
+            book.AddGrade(92.3);
+
+            // creates a new List that would contain double data types
+            var grades = new List<double>() {12.5,69.2,42.1,21.4};
+            grades.Add(42.0);
+            var sum = 0.0;
+            
+            foreach (var grade in grades)
+            {
+                sum += grade;
+            }
+            var ave = sum / grades.Count;
+            System.Console.WriteLine($"Your total grade is {sum:N0} with an average of {ave:N2}");
         }
     }
 }
