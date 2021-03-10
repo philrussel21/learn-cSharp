8590591c5f05538f298f7ecc74d2dfd3485b0bba
philrussel21 <phil.antiporda21@gmail.com>
2021-03-10 16:54:58 +1000

    Adds a test to show that C# is pass by value when passing args as params to methods

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -7,6 +7,34 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
         public void GetBookReturnsDifferentObjects()
         {
             var book1 = GetBook("Book 1");
